632e2953a7833b4b24941206fd2e1bea9301de31
andrek <zblackoez@gmail.com>
2023-04-16 18:13:38 -0400

    Criando o inicio, e adicionando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SobreComponent } from './page/sobre/sobre.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { ConteudoComponent } from './navegacao/conteudo/conteudo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreComponent,
+    MenuComponent,
+    FooterComponent,
+    ConteudoComponent
   ],
   imports: [
     BrowserModule,
